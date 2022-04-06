a4718c2e35b4534b3867d9c3f56fc0618086617d
SharonJseg <sharon.j.segal@gmail.com>
2022-04-06 14:14:09 +0300

    styling continues

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,12 @@ import './App.css';
 import logo from '../../images/logo.svg';
 import curvedline from '../../images/pattern-curved-line-1.svg';
 import heroImage from '../../images/image-hero-portrait@2x.webp';
+import cmoImage from '../../images/image-jeremy-large@2x.webp';
+import patternBlur from '../../images/pattern-blur.svg';
+import smallLine from '../../images/pattern-curved-line-2.svg';
+import fb from '../../images/icon-facebook.svg';
+import tw from '../../images/icon-twitter.svg';
+import inst from '../../images/icon-instagram.svg';
 
 function App() {
   return (
@@ -13,7 +19,7 @@ function App() {
         <button className='request-beta_secondary'>Request Beta Access</button>
       </header>
 
-      <main>
+      <main className='main'>
         <section className='hero'>
           <div className='hero__cta'>
             <div className='hero__cta-header-container'>
@@ -59,7 +65,59 @@ function App() {
             </ul>
           </div>
         </section>
+
+        <section className='testimonial'>
+          <div className='testimonial__image-container'>
+            <img
+              className='testimonial__image'
+              src={cmoImage}
+              alt='jeremy robinson'
+            />
+            <img className='testimonial__pattern' src={patternBlur} alt='' />
+          </div>
+          <div className='testimonial__curvedLine'>
+            <img src={smallLine} alt='' />
+          </div>
+          <div className='testimonial__content'>
+            <h2 className='testimonial__content-title'>
+              <span className='testimonial__content-title_regular'>
+                it just{' '}
+              </span>
+              works.
+            </h2>
+            <figure className='testimonial__content-quote-container'>
+              <blockquote className='testimonial__content-quote'>
+                “I really like how it is an all-in-one solution that handle many
+                of the tasks that you would normally need separate tools to do
+                the same job. This thing is a miracle worker.”
+              </blockquote>
+              <figcaption className='testimonial__content-caption'>
+                JEREMY ROBINSON
+                <span className='testimonial__content-caption_span'>
+                  CMO, FYLO
+                </span>
+              </figcaption>
+            </figure>
+          </div>
+        </section>
       </main>
+      <footer className='footer'>
+        <div className='footer__container'>
+          <img className='footer__logo' src={logo} alt='footer logo' />
+          <p className='footer__copyright'>Copyright - Suite</p>
+          <ul className='footer__social-icons'>
+            <li>
+              <img src={fb} alt='' className='social-icon' />
+            </li>
+            <li>
+              <img src={tw} alt='' className='social-icon' />
+            </li>
+            <li>
+              <img src={inst} alt='' className='social-icon' />
+            </li>
+          </ul>
+        </div>
+      </footer>
     </div>
   );
 }
